b724870da494747208e2678ba706a6bbceb0eea4
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-08 16:41:41 -0500

    commit countLetters

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const countLetters = function(str) {
+  const result = {};
+  for (const letter of str) {
+    if (result[letter]) {
+      result[letter]++;
+    } else {
+      result[letter] = 0;
+      result[letter]++;
+    }
+  }
+  return result;
+};
+console.log(countLetters('lighthouse'));
