c32122330893b2ad461f2aedf8bca2af0f3f6d27
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-08 17:11:11 -0500

    Commit letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const keys = sentence.charAt(i);
+    if (!results[keys]) {
+      results[keys] = [];
+      results[keys].push(i);
+    } else {
+      results[keys].push(i);
+    }
+  }
+  return results;
+};
+console.log(letterPositions('hello how are you today'));
