16ff0484efd88131a9758e303cd16f9b6875147f
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-09 18:10:02 -0500

    commit takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,20 @@
+const takeUntil = function(array, callback) {
+  const arrBuff = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      break;
+    }
+    arrBuff.push(array[i]);
+  }
+  return arrBuff;
+};
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
