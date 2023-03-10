f72ab0a5d88b9f3c435297ab045aeffda6b62cc7
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-09 20:10:30 -0500

    Find Key

---
+++ findKey.js
@@ -0,0 +1,22 @@
+const findKey = function(someObj, someFunc) {
+
+  for (const prop in someObj) {
+    if (someFunc(someObj[prop])) {
+      return prop;
+    }
+  }
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+console.log(result);
+
+
