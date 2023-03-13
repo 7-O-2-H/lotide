b101466d7ee537096c8a289257da9b8c9019e4ce
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-13 15:12:06 -0400

    commit tail and tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,5 @@
 const tail = require('../tail');
 const assertArraysEqual = require('../assertArrayEqual');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
 
 assertArraysEqual(result, ["Lighthouse", "Labs"]);
